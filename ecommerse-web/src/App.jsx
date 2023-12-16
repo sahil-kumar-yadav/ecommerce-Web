f19d775dadac3be1ce7099e76a1ddc2237d91eb9
yadavsk098 <yadavsk098@gmail.com>
2023-12-16 20:17:08 +0530

    uploaded files

---
+++ ecommerse-web/src/App.jsx
@@ -0,0 +1,24 @@
+// rafc for boilerplate
+import React from 'react'
+// import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+// import Product from './components/Product'
+// import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+// import ProductDetail from './components/ProductDetail'
+// import SearchItem from './components/SearchItem'
+// import Cart from './components/Cart'
+// import { items } from './components/Data'
+
+
+const App = () => {
+  // const [data, setData] = useState([...items])
+  // const [cart, setCart] = useState([])
+  return (
+    <>
+    <Navbar />
+    
+    </>
+  )
+}
+
+export default App
---
+++ ecommerse-web/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <>
+            <header>
+                <div className="nav-bar">
+                    <div className="brand">E-cart</div>
+                    <div className="search-bar">
+                        <input type="text" placeholder='Search Product' />
+                    </div>
+                    <div className="cart">Cart</div>
+                </div>
+
+                <div className="nav-bar-wrapper">
+                    <div className="items">Filter by{"->"}</div>
+                    <div className="items">No Filter</div>
+                    <div className="items">Mobiles</div>
+                    <div className="items">Laptops</div>
+                    <div className="items">{">="}Tablets</div>
+                    <div className="items">{">="}29999</div>
+                    <div className="items">{">="}39999</div>
+                    <div className="items">{">="}49999</div>
+                    <div className="items">{">="}59999</div>
+                </div>
+
+            </header>
+        </>
+    )
+}
+
+export default Navbar
